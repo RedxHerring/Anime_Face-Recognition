e96108a5db9d4f9b080fdd38c6b8b368c0545e50
redxhering <hpowell@umass.edu>
2023-04-25 11:32:54 -0400

    mm experiments

---
+++ aniFR-setup.sh
@@ -0,0 +1,13 @@
+# run this to create conda environment and install special dependencies within
+
+conda create -n aniFR python=3.10.10 -y
+conda activate aniFR
+conda install pytorch torchvision -c pytorch -y
+pip install -U openmim
+mim install mmengine
+mim install "mmcv>=2.0.0"
+mim install mmdet
+
+# For verification
+mim download mmdet --config rtmdet_tiny_8xb32-300e_coco --dest .
+python testmm.py
