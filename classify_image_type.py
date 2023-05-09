ca18555913ea6ce4300b582dbb5f7b8eed1a258c
redxhering <hpowell@umass.edu>
2023-05-09 17:07:07 -0400

    added cuda

--- classify_image_type.py
+++ classify_image_type.py
@@ -3,6 +3,10 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+# Enable GPU acceleration
+physical_devices = tf.config.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 # Set up the CNN model
 model = keras.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(96, 96, 3)),
@@ -13,6 +17,7 @@ model = keras.Sequential([
     layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
     layers.Dense(3, activation='softmax')
 ])
 
@@ -31,7 +36,7 @@ datagen = ImageDataGenerator(
 
 # Load and preprocess the dataset
 batch_size = 32
-train_data_dir = 'datasets'
+train_data_dir = 'datasetsTON'
 train_generator = datagen.flow_from_directory(
     train_data_dir,
     target_size=(96, 96),
